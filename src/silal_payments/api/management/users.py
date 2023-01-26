e58a3042b8a7d26682c719a8e618b19ce3bb6eab
Ibraheem Alyan <35265448+ibraheemalayan@users.noreply.github.com>
2023-01-26 15:13:30 +0200

    build:  🗃️ added user model

---
+++ src/silal_payments/api/management/users.py
@@ -0,0 +1,17 @@
+from flask import url_for, redirect, render_template
+from . import management_api
+from silal_payments.models.user import User, load_user_from_db
+
+
+@management_api.route(
+    "/user_profile/<int:user_id>/", methods=["GET"], subdomain="management"
+)
+def get_user_by_id(user_id: int):
+    """index"""
+
+    user: User = load_user_from_db(user_id=user_id)
+
+    if user is None:
+        return redirect(url_for("shared_api.not_found"))
+
+    return render_template("user_profile.html", user=user)
