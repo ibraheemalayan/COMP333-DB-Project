fa4aaf38a1c95ea67d61f407f687bf97c648e85b
Ibraheem Alyan <35265448+ibraheemalayan@users.noreply.github.com>
2023-01-28 19:18:35 +0200

    feat: ✨ transaction model and data

--- src/silal_payments/api/management/admin.py
+++ src/silal_payments/api/management/admin.py
@@ -1,11 +1,11 @@
+from silal_payments.auth.decorators import manager_login_required
 from . import management_api
 from flask import render_template
-from flask_login import login_required
 
 
-@management_api.route("/")
+@management_api.route("/", methods=["GET"], subdomain="management")
 @management_api.route("/home/", methods=["GET"], subdomain="management")
-@login_required
+@manager_login_required
 def home():
     """index"""
 
