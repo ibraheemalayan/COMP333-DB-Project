7f6eeb368ec9b2cd45ebe7fe0e7ef7975b47f190
مُحَمَّــد أبو شِلبايَة <mabushelbaia@gmail.com>
2023-02-04 13:52:59 +0200

    feat: ⚙️ Added load_products

--- src/silal_payments/models/product.py
+++ src/silal_payments/models/product.py
@@ -2,13 +2,15 @@ from sqlalchemy import text
 from silal_payments import db
 from sqlalchemy.engine import Result, Row
 
+
 class Product:
+    table_name = "product"
     def __init__(self, product_id: int, product_name: str, product_price: int, product_seller: id) -> None:
         self.product_id: int = product_id
         self.product_name: str = product_name
         self.product_price: int = product_price
         self.product_seller: id = product_seller
-    
+
     def insert_into_db(self) -> int:
         product_id: Row = db.session.execute(
             text(
@@ -25,3 +27,13 @@ class Product:
 
     def __str__(self) -> str:
         return f"""Product: product_id={self.product_id} product_name={self.product_name} product_price={self.product_price} product_seller={self.product_seller}"""
+
+    def load_products_from_db():
+        result = db.session.execute(
+            text(f"""SELECT * FROM public.{Product.table_name}""")
+        )
+
+        products = [Product(product_id=row[0], product_name=row[1], product_price=row[2], product_seller=row[3]) for row in result]
+
+
+        return products
