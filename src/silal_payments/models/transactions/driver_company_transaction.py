95daeb5d3c1b4457b2170cb2b68f002ba83e9056
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2023-02-06 14:21:26 +0200

    feat: ✨ seller_company_transaction_load_details

--- src/silal_payments/models/transactions/driver_company_transaction.py
+++ src/silal_payments/models/transactions/driver_company_transaction.py
@@ -3,6 +3,7 @@ from silal_payments import db
 from sqlalchemy import text
 from sqlalchemy.engine import Result, Row
 from silal_payments.models.transactions.transaction import Transaction, TransactionType
+from silal_payments.models.users.driver import Driver
 
 from datetime import datetime
 
@@ -38,32 +39,76 @@ class DriverCompanyTransaction(Transaction):
         db.session.commit()
 
 
-def load_driver_company_transaction_from_db(
-    transaction_id: int,
-) -> DriverCompanyTransaction:
-    """Load a driver_company_transaction from the database"""
+# def load_driver_company_transaction_from_db(
+#     transaction_id: int,
+# ) -> DriverCompanyTransaction:
+#     """Load a driver_company_transaction from the database"""
+#     stmt = text(
+#         f"""
+#         SELECT
+#             public.{DriverCompanyTransaction.sub_table_name}.transaction_id,
+#             public.{Transaction.table_name}.transaction_amount,
+#             public.{Transaction.table_name}.transaction_date,
+#             public.{DriverCompanyTransaction.sub_table_name}.driver_id
+#         FROM public.{DriverCompanyTransaction.sub_table_name}
+#         INNER JOIN public.{Transaction.table_name}
+#         ON public.{DriverCompanyTransaction.sub_table_name}.transaction_id = public.{Transaction.table_name}.transaction_id
+#         WHERE public.{DriverCompanyTransaction.sub_table_name}.transaction_id = :transaction_id;
+#         """
+#     ).bindparams(transaction_id=transaction_id)
+#     result: Result = db.session.execute(stmt)
+#     transaction: Row = result.first()
+
+#     if transaction is None:
+#         return None
+
+#     return DriverCompanyTransaction(
+#         transaction_id=transaction[0],
+#         transaction_amount=transaction[1],
+#         transaction_date=transaction[2],
+#         driver_id=transaction[3],
+#     )
+
+
+def load_driver_company_transaction_details(transaction_id: int) -> tuple:
+    """load a driver_company_transaction from the database"""
+
     stmt = text(
         f"""
         SELECT
-            public.{DriverCompanyTransaction.sub_table_name}.transaction_id,
-            public.{Transaction.table_name}.transaction_amount,
-            public.{Transaction.table_name}.transaction_date,
-            public.{DriverCompanyTransaction.sub_table_name}.driver_id
+        public.{Driver.table_name}.user_id,
+        public.{Driver.table_name}.full_name,
+        public.{DriverCompanyTransaction.sub_table_name}.transaction_id,
+        public.{Transaction.table_name}.transaction_amount,
+        public.{Transaction.table_name}.transaction_date
         FROM public.{DriverCompanyTransaction.sub_table_name}
         INNER JOIN public.{Transaction.table_name}
         ON public.{DriverCompanyTransaction.sub_table_name}.transaction_id = public.{Transaction.table_name}.transaction_id
+        INNER JOIN public.{Driver.table_name}
+        ON public.{DriverCompanyTransaction.sub_table_name}.driver_id = public.{Driver.table_name}.user_id
         WHERE public.{DriverCompanyTransaction.sub_table_name}.transaction_id = :transaction_id;
         """
     ).bindparams(transaction_id=transaction_id)
+
     result: Result = db.session.execute(stmt)
     transaction: Row = result.first()
 
     if transaction is None:
         return None
 
-    return DriverCompanyTransaction(
-        transaction_id=transaction[0],
-        transaction_amount=transaction[1],
-        transaction_date=transaction[2],
-        driver_id=transaction[3],
+    return (
+        Driver(
+            user_id=transaction[0],
+            full_name=transaction[1],
+            phone=None,
+            bank_account=None,
+            password_hash=None,
+            email=None,
+        ),
+        DriverCompanyTransaction(
+            transaction_id=transaction[2],
+            transaction_amount=transaction[3],
+            transaction_date=transaction[4],
+            driver_id=transaction[0],
+        ),
     )
--- src/silal_payments/models/transactions/seller_company_transaction.py
+++ src/silal_payments/models/transactions/seller_company_transaction.py
@@ -3,6 +3,7 @@ from silal_payments import db
 from sqlalchemy import text
 from sqlalchemy.engine import Result, Row
 from silal_payments.models.transactions.transaction import Transaction, TransactionType
+from silal_payments.models.users.seller import Seller
 
 import datetime
 
@@ -38,32 +39,76 @@ class SellerCompanyTransaction(Transaction):
         db.session.commit()
 
 
-def load_seller_company_transaction_from_db(
-    transaction_id: int,
-) -> SellerCompanyTransaction:
-    """Load a seller_company_transaction from the database"""
+# def load_seller_company_transaction_from_db(
+#     transaction_id: int,
+# ) -> SellerCompanyTransaction:
+#     """Load a seller_company_transaction from the database"""
+#     stmt = text(
+#         f"""
+#         SELECT
+#             public.{SellerCompanyTransaction.sub_table_name}.transaction_id,
+#             public.{Transaction.table_name}.transaction_amount,
+#             public.{Transaction.table_name}.transaction_date,
+#             public.{SellerCompanyTransaction.sub_table_name}.seller_id
+#         FROM public.{SellerCompanyTransaction.sub_table_name}
+#         INNER JOIN public.{Transaction.table_name}
+#         ON public.{SellerCompanyTransaction.sub_table_name}.transaction_id = public.{Transaction.table_name}.transaction_id
+#         WHERE public.{SellerCompanyTransaction.sub_table_name}.transaction_id = :transaction_id;
+#         """
+#     ).bindparams(transaction_id=transaction_id)
+#     result: Result = db.session.execute(stmt)
+#     transaction: Row = result.first()
+
+#     if transaction is None:
+#         return None
+
+#     return SellerCompanyTransaction(
+#         transaction_id=transaction[0],
+#         transaction_amount=transaction[1],
+#         transaction_date=transaction[2],
+#         seller_id=transaction[3],
+#     )
+
+
+def load_seller_company_transactions_details(transaction_id: int) -> tuple:
+    """load seller company transaction details from the database"""
+
     stmt = text(
         f"""
         SELECT
+            public.{Seller.table_name}.user_id,
+            public.{Seller.table_name}.full_name,
             public.{SellerCompanyTransaction.sub_table_name}.transaction_id,
             public.{Transaction.table_name}.transaction_amount,
-            public.{Transaction.table_name}.transaction_date,
-            public.{SellerCompanyTransaction.sub_table_name}.seller_id
+            public.{Transaction.table_name}.transaction_date
         FROM public.{SellerCompanyTransaction.sub_table_name}
         INNER JOIN public.{Transaction.table_name}
         ON public.{SellerCompanyTransaction.sub_table_name}.transaction_id = public.{Transaction.table_name}.transaction_id
+        INNER JOIN public.{Seller.table_name}
+        ON public.{SellerCompanyTransaction.sub_table_name}.seller_id = public.{Seller.table_name}.user_id
         WHERE public.{SellerCompanyTransaction.sub_table_name}.transaction_id = :transaction_id;
         """
     ).bindparams(transaction_id=transaction_id)
+
     result: Result = db.session.execute(stmt)
     transaction: Row = result.first()
 
     if transaction is None:
         return None
 
-    return SellerCompanyTransaction(
-        transaction_id=transaction[0],
-        transaction_amount=transaction[1],
-        transaction_date=transaction[2],
-        seller_id=transaction[3],
+    return (
+        Seller(
+            user_id=transaction[0],
+            full_name=transaction[1],
+            phone=None,
+            password_hash=None,
+            email=None,
+            bank_account=None,
+        ),
+        SellerCompanyTransaction(
+            transaction_id=transaction[2],
+            transaction_amount=transaction[3],
+            transaction_date=transaction[4],
+            seller_id=transaction[0],
+        ),
     )
