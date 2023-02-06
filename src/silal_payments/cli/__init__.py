982fcc9b33aaec7a98ec5c4c3546095454022104
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2023-02-06 12:38:10 +0200

    feat: ✨ company driver transaction query

--- src/silal_payments/cli/__init__.py
+++ src/silal_payments/cli/__init__.py
@@ -1,6 +1,13 @@
 from flask import Blueprint, current_app
 from sqlalchemy import text
 from silal_payments.models.product import Product
+from silal_payments.models.transactions.company_driver_transaction import (
+    CompanyDriverTransaction,
+    load_company_driver_transaction_from_db,
+    load_transaction_details,
+)
+from datetime import datetime
+
 
 cli_bp = Blueprint("cli", __name__)
 
@@ -13,11 +20,24 @@ def init_db():
 @cli_bp.cli.command("db-fill")
 def init_db():
     from silal_payments.db_mig import fill_defaults_and_fake
-    
+
+
 @cli_bp.cli.command("test-products")
 def test():
     for product in Product.load_products_from_db():
         print(product)
+
+
+@cli_bp.cli.command("test-company-driver-transaction")
+def test_t():
+
+    info = load_transaction_details(transaction_id=26)
+    print("_" * 20)
+    print(info[0])
+    print("_" * 20)
+    print(info[1])
+
+
 @cli_bp.cli.command("drop-db")
 def drop_db():
 
@@ -41,8 +61,6 @@ def drop_db():
 # def init_db_with_sample_data():
 #     from silal.db_mig import initialize_db
 #     from silal.db_mig import fake_fill_db
-
-
 # TODO Never make this runnable in production
 @cli_bp.cli.command("reset-db-w-sample-data")
 def init_db_with_sample_data():
