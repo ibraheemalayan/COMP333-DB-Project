05f47dead482c8f63f4574e0e377cdb51bacd02f
Ibraheem Alyan <35265448+ibraheemalayan@users.noreply.github.com>
2023-02-06 15:10:52 +0200

    fix: 🎨 fix missing fees in order

--- src/silal_payments/api/management/transactions.py
+++ src/silal_payments/api/management/transactions.py
@@ -2,6 +2,9 @@ from silal_payments.auth.decorators import manager_login_required
 from silal_payments.models.transactions.company_driver_transaction import (
     load_company_driver_transaction_details,
 )
+from silal_payments.models.transactions.customer_company_transaction import (
+    load_customer_company_transaction_details,
+)
 from silal_payments.models.users.driver import Driver
 from . import management_api
 from flask import render_template
@@ -20,4 +23,20 @@ def company_driver_transactions(t_id):
         "management/transaction_details/company_driver_transaction_details.html",
         transaction=transaction,
         driver=driver,
+    )
+
+
+@management_api.route(
+    "/transaction/customer_company/<int:t_id>/", methods=["GET"], subdomain="management"
+)
+@manager_login_required
+def customer_company_transactions(t_id):
+    """transactions details"""
+
+    customer, transaction = load_customer_company_transaction_details(t_id)
+
+    return render_template(
+        "management/transaction_details/customer_company_transaction_details.html",
+        transaction=transaction,
+        customer=customer,
     )
