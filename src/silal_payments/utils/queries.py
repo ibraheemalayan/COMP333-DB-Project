3523360321473c53802c64485f6e6ff9a5c19e59
Dr.NIDO <104252621+NidalZabade@users.noreply.github.com>
2023-02-06 15:11:44 +0200

    feat: edit queries

--- src/silal_payments/utils/queries.py
+++ src/silal_payments/utils/queries.py
@@ -12,35 +12,55 @@ from silal_payments.models.users.driver import Driver
 
 # define a class for the result
 
+
 class Item:
-    def __init__(self, product, seller_name, quantity, order_price):
+    def __init__(self, product, seller_name, quantity, order_price, delivery_fee):
         self.product: Product = product
         self.seller_name: str = seller_name
         self.quantity: int = quantity
         self.order_price: float = order_price
+        self.delivery_fee: float = delivery_fee
+
     def __str__(self):
-        return f"""Item: product={self.product.__str__()} seller={self.seller_name} quantity={self.quantity}, Order-Price: {self.order_price},  total={self.total()}"""
+        return f"""Item: product={self.product.__str__()} seller={self.seller_name} quantity={self.quantity}, Order-Price: {self.order_price},  total={self.total(), self.delivery_fee}"""
 
     def total(self):
         return self.order_price * self.quantity
 
 
 def showOrderProducts(order_id):
-    result = db.session.execute(text(
-        f"""	
-	select pr.*, u.full_name, oi.quantity, oi.price_per_unit
-	FROM 
-	public.product pr JOIN public.order_item oi 
-	ON oi.product_id = pr.product_id 
-	JOIN public.seller s 
+    result = db.session.execute(
+        text(
+            f"""
+	select pr.*, u.full_name, oi.quantity, oi.price_per_unit, o.delivery_fee
+	FROM
+	public.product pr JOIN public.order_item oi
+	ON oi.product_id = pr.product_id
+	JOIN public.seller s
 	ON pr.product_seller = s.user_id
 	JOIN public.user u
 	on u.user_id = s.user_id
+    JOIN public.order o
+    on o.order_id = oi.order_id
     WHERE oi.order_id = :oid
+
     """
-    ).bindparams(oid=order_id))
+        ).bindparams(oid=order_id)
+    )
     Items = []
     for row in result:
-        Items.append(Item(Product(product_id=row[0], product_name=row[1],
-                     product_price=row[2], product_seller=row[3]), row[4], row[5], row[6]))
+        Items.append(
+            Item(
+                Product(
+                    product_id=row[0],
+                    product_name=row[1],
+                    product_price=row[2],
+                    product_seller=row[3],
+                ),
+                row[4],
+                row[5],
+                row[6],
+                row[7],
+            )
+        )
     return Items
