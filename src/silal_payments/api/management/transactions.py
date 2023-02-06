6c650cfb35be756103bd0f7163937da819845468
Ibraheem Alyan <35265448+ibraheemalayan@users.noreply.github.com>
2023-02-06 16:53:08 +0200

    refactor: ♻️ refactor transaction table APIs

--- src/silal_payments/api/management/transactions.py
+++ src/silal_payments/api/management/transactions.py
@@ -5,9 +5,44 @@ from silal_payments.models.transactions.company_driver_transaction import (
 from silal_payments.models.transactions.customer_company_transaction import (
     load_customer_company_transaction_details,
 )
+from silal_payments.models.transactions.transaction import (
+    TransactionType,
+    load_transactions_from_db,
+)
 from silal_payments.models.users.driver import Driver
 from . import management_api
 from flask import render_template
+
+
+@management_api.route("/transactions/", methods=["GET"], subdomain="management")
+@manager_login_required
+def transactions():
+    """list transactions"""
+
+    transactions: list[TransactionType] = load_transactions_from_db()
+
+    transaction_types_bs_classes = {
+        "customer_driver_transaction": "success",
+        "customer_company_transaction": "danger",
+        "company_driver_transaction": "primary",
+        "seller_company_transaction": "secondary",
+        "driver_company_transaction": "info",
+    }
+
+    transaction_link_prefix = {
+        "customer_driver_transaction": "",
+        "customer_company_transaction": "customer_company",
+        "company_driver_transaction": "company_driver",
+        "seller_company_transaction": "",
+        "driver_company_transaction": "",
+    }
+
+    return render_template(
+        "management/transaction_list.html",
+        transactions=transactions,
+        transaction_types_bs_classes=transaction_types_bs_classes,
+        transaction_link_prefix=transaction_link_prefix,
+    )
 
 
 @management_api.route(
